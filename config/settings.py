f1c09d5bf2598c91b147bf315d9e741fe927545b
johoseok <kmb5037@naver.com>
2024-07-14 21:49:05 +0900

    add static_root

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-oa^2)ie2ys@tvy8)4=hqma8_hz^3$#z6jw9_n70vzapp)x9@_x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['juhoseok.pythonanywhere.com']
+
 
 
 # Application definition
@@ -119,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
